6dc15ff3c191be199f7a8a306cc174ae4f2433bf
Anubhav Bhattarai <mail@theanubhavb.com>
2025-10-17 00:22:50 +0545

    Initial commit - NRB Forex Add-in

---
+++ node_modules/custom-functions-metadata/Specification.md
@@ -0,0 +1,217 @@
+# Custom Functions Metadata Specification
+
+## Overview
+
+When an Excel custom function is written in JavaScript or TypeScript, JSDoc tags are used to provide the extra information about the custom function.
+
+Add the `@customfunction` tag in the comments for a JavaScript or TypeScript function to mark it as a custom function. 
+
+The function parameter types may be provided using the [@param](#param) tag in JavaScript, or from the [Function type](http://www.typescriptlang.org/docs/handbook/functions.html) in TypeScript. For more information, see the [@param](#param) tag and [Types](#Types) section.
+
+## Tags
+* [@cancelable](#cancelable)
+* [@capturesCallingObject](#capturescallingobject)
+* [@customfunction](#customfunction) id name
+* [@excludeFromAutoComplete](#excludeFromAutoComplete)
+* [@helpurl](#helpurl) url
+* [@linkedEntityLoadService](#linkedEntityLoadService)
+* [@param](#param) _{type}_ name description
+* [@requiresAddress](#requiresAddress)
+* [@requiresParameterAddresses](#requiresParameterAddresses)
+* [@returns](#returns) _{type}_
+* [@streaming](#streaming)
+* [@volatile](#volatile)
+
+---
+### @cancelable
+
+Indicates that a custom function wants to perform an action when the function is canceled. 
+
+The last function parameter must be of type `CustomFunctions.CancelableInvocation`. The function can assign a function to the `oncanceled` property to denote the action to perform when the function is canceled.
+
+If the last function parameter is of type `CustomFunctions.CancelableInvocation`, it will be considered `@cancelable` even if the tag is not present.
+
+ A function cannot have both `@cancelable` and `@streaming` tags.
+
+---
+### @capturesCallingObject
+
+Indicates that the custom function captures the calling object i.e. the [Excel.CellValue](https://learn.microsoft.com/es-es/javascript/api/excel/excel.cellvalue)
+object that invoked the custom function, in the first parameter.
+
+---
+### @customfunction
+
+Syntax: @customfunction _id_ _name_
+
+Specify this tag to treat the JavaScript/TypeScript function as an Excel custom function.
+
+This tag is required to generate metadata for the custom function.
+
+There should also be a call to `CustomFunctions.associate("id", functionName);`
+
+#### id 
+
+The id is used as the invariant identifier for the custom function stored in the document. It should not change.
+
+* If id is not provided, the JavaScript/TypeScript function name is converted to uppercase, disallowed characters are removed.
+* The id must be unique for all custom functions.
+* The characters allowed are limited to: A-Z, a-z, 0-9, underscore (_), and period (.).
+
+#### name
+
+Provides the display name for the custom function. 
+
+* If name is not provided, the id is also used as the name.
+* Allowed characters: Letters [Unicode Alphabetic character](https://www.unicode.org/reports/tr44/tr44-22.html#Alphabetic), numbers, period (.), and underscore (\_).
+* Must start with a letter.
+* Maximum length is 128 characters.
+
+---
+### @excludeFromAutoComplete
+
+Indicates that the function will be excluded from the autocomplete drop-down list and Formula Builder.
+
+If the function is manually spelled correctly in the grid, the function will still execute.
+
+A function cannot have both `@excludeFromAutoComplete` and `@linkedEntityLoadService` tags.
+
+---
+### @helpurl
+
+Syntax: @helpurl _url_
+
+The provided _url_ is displayed in Excel.
+
+---
+### @linkedEntityLoadService
+
+Indicates that the function is a "special" custom function that is meant to act as the "loadFunction" for user defined `LinkedEntityDataDomain`s. 
+
+The function will be excluded from the autocomplete drop-down list and Formula Builder since it should only callable by the Excel runtime.
+
+* Must accept and return a single non-repeating, non-optional, scalar parameter of type `unknown`.
+* Must not be a XLL-compatible custom function.
+* Must allow rich data as input.
+* A `@linkedEntityLoadService` function cannot be combined with `@streaming`, `@volatile`, `@requiresAddress`, `@requiresParameterAddresses`, `@excludeFromAutoComplete`, or `@capturesCallingObject` tags.
+
+---
+### @param 
+
+#### JavaScript
+
+JavaScript Syntax: @param {type} name _description_
+
+* `{type}` should specify the type info within curly braces. See the [Types](##types) for more information about the types which may be used. Optional: if not specified, the type `any` will be used.
+* `name` specifies which parameter the @param tag applies to. Required.
+* `description` provides the description which appears in Excel for the function parameter. Optional.
+
+To denote a custom function parameter as optional:
+* Put square brackets around the parameter name. For example: `@param {string} [text] Optional text`.
+
+#### TypeScript
+
+TypeScript Syntax: @param name _description_
+
+* `name` specifies which parameter the @param tag applies to. Required.
+* `description` provides the description which appears in Excel for the function parameter. Optional.
+
+See the [Types](##types) for more information about the function parameter types which may be used.
+
+To denote a custom function parameter as optional, do one of the following:
+* Use an optional parameter. For example: `function f(text?: string)`
+* Give the parameter a default value. For example: `function f(text: string = "abc")`
+
+For detailed description of the @param see: [JSDoc](http://usejsdoc.org/tags-param.html)
+
+---
+### @requiresAddress
+
+Indicates that the address of the cell where the function is being evaluated should be provided. 
+
+The last function parameter must be of type `CustomFunctions.Invocation` or a derived type. When the function is called, the `address` property will contain the address.
+
+---
+### @requiresParameterAddresses
+
+Indicates that the address of the input parameters where the function is being evaluated should be provided. 
+
+The last function parameter must be of type `CustomFunctions.Invocation` or a derived type. When the function is called, the `parameterAddresses` property will contain an array containing the address of each parameter.
+
+---
+### @returns
+
+Syntax: @returns {_type_}
+
+Provides the type for the return value.
+
+If `{type}` is omitted, the TypeScript type info will be used. If there is no type info, the type will be `any`.
+
+---
+### @streaming
+
+Used to indicate that a custom function is a streaming function. 
+
+The last parameter should be of type `CustomFunctions.StreamingInvocation<ResultType>`.
+The function should return `void`.
+
+Streaming functions do not return values directly, but rather should call `setResult(result: ResultType)` using the last parameter.
+
+Exceptions thrown by a streaming function are ignored. `setResult()` may be called with Error to indicate an error result.
+
+Streaming functions cannot be marked as [@volatile](#volatile).
+
+---
+### @volatile
+
+A volatile function is one whose result cannot be assumed to be the same from one moment to the next even if it takes no arguments or the arguments have not changed. Excel re-evaluates cells that contain volatile functions, together with all dependents, every time that a calculation is done. For this reason, too much reliance on volatile functions can make recalculation times slow, so use them sparingly.
+
+Streaming functions cannot be volatile.
+
+---
+
+## Types
+
+By specifying a parameter type, Excel will convert values into that type before calling the function. If the type is `any`, no conversion will be performed.
+
+### Value types
+
+A single value may be represented using one of the following types: `boolean`, `number`, `string`.
+
+### Matrix type
+
+Use a two-dimensional array type to have the parameter or return value be a matrix of values. For example, the type `number[][]` indicates a matrix of numbers. `string[][]` indicates a matrix of strings. 
+
+### Error type
+
+A non-streaming function can indicate an error by returning an Error type.
+
+A streaming function can indicate an error by calling setResult() with an Error type.
+
+### Promise
+
+A function can return a Promise, which will provide the value when the promise is resolved. If the promise is rejected, then it is an error.
+
+### Other types
+
+Any other type will be treated as an error.
+
+---
+## Custom enums
+
+The `@customEnum` JSDoc tag can be used in TypeScript to mark an enum as a custom enum. When a custom function uses this custom enum as the type for its arguments, the enum values will be displayed in a dropdown list for the end user when they enter the function.
+
+### Tag @customEnum
+
+Syntaxt: @customEnum _{type}_
+
+Specify this tag to mark the TypeScript enum as an Excel custom enum. 
+
+This tag is required to generate metadata for the custom enum.
+
+* `{type}` should specify the type information within curly braces. Accepted types are `string` and `number`. Optional: if not specified, the type of the first enum value will be used.
+* Only TypeScript is supported, as JavaScript does not support enums.
+* The enum name must start with an alphabetic character and be unique. Allowed characters are: A-Z, a-z, 0-9, underscore (_), and period (.).
+* The maximum length of an enum name is 128 characters.
+* If values of enum members are omitted, the first member will be assigned a numeric value of `0`, with subsequent values auto-incremented.
+* The types of enum members must be consistent. Mixed types within an enum will be treated as an error.
---
+++ src/functions/functions.ts
@@ -0,0 +1,298 @@
+﻿// =====================================================================================
+// Custom Function Namespace: NRB
+// =====================================================================================
+
+/**
+ * Test NRB API Connection
+ * @customfunction
+ * @returns {Promise<string>} Connection status
+ * @alias NRB.TESTCONNECTION
+ */
+export async function TestConnection(): Promise<string> {
+    const testUrl = `https://www.nrb.org.np/api/forex/v1/rates?from=2024-01-01&to=2024-01-01&page=1&per_page=1`;
+
+    try {
+        const response = await fetch(testUrl, {
+            headers: { 'User-Agent': 'Excel-NRB-Addon' } 
+        });
+
+        if (response.ok) {
+            const jsonResponse = await response.json();
+
+            // FIX: correctly extract nested status.code if present
+            const status = jsonResponse.status?.code ?? jsonResponse.status;
+
+            if (status === 200 || status === 1) {
+                return "SUCCESS: NRB API is reachable and responded with status 200/1.";
+            }
+
+            // Otherwise include debug details
+            const debugDetail = JSON.stringify(jsonResponse, null, 2);
+            return `API WARNING: HTTP OK but unexpected status (${status}). Response snippet:\n${debugDetail.substring(0, 500)}...`;
+        }
+
+        // HTTP errors
+        return `NETWORK ERROR: HTTP Status ${response.status} - ${response.statusText}`;
+
+    } catch (e) {
+        return `ERROR: Could not complete request. (${e instanceof Error ? e.message : String(e)})`;
+    }
+}
+
+
+/**
+ * Get NRB Forex Rate (smart single/range return)
+ * @customfunction
+ * @param {any} fromDate Start date
+ * @param {any} [toDate] End date (optional)
+ * @param {string} [currencyPair="USDNPR"] Currency pair (default: USDNPR)
+ * @param {string} [rateType="S"] B for Buy, S for Sell (default: S)
+ * @returns {Promise<string | number>} Forex rate (number for single date, JSON string for range)
+ * @alias NRB.FOREXRATE
+ */
+export async function ForexRate(
+    fromDate: any,
+    toDate?: any,
+    currencyPair: string = "USDNPR",
+    rateType: string = "S"
+): Promise<string | number> {
+    try {
+        if (!fromDate) return "#ERROR: From Date is required";
+
+        const requestedFromStr = formatDate(fromDate);
+        if (requestedFromStr === "ERROR") return "#ERROR: Invalid From Date";
+
+        let toStr = requestedFromStr;
+        let isRange = false;
+
+        if (toDate !== null && toDate !== undefined && toDate !== "") {
+            toStr = formatDate(toDate);
+            if (toStr === "ERROR") return "#ERROR: Invalid To Date";
+            isRange = requestedFromStr !== toStr;
+        }
+
+        currencyPair = String(currencyPair || "USDNPR").trim().toUpperCase();
+        if (currencyPair.length !== 6) return "#ERROR: Invalid currency (e.g., USDNPR)";
+
+        const needsInversion = currencyPair.startsWith("NPR");
+        const targetCurrency = needsInversion ? currencyPair.slice(3) : currencyPair.slice(0, 3);
+
+        rateType = String(rateType || "S").trim().toUpperCase();
+        if (!["B", "S"].includes(rateType)) return "#ERROR: Rate type must be 'B' or 'S'";
+
+        // =================== RANGE HANDLING ===================
+        if (isRange) {
+            const url = `https://www.nrb.org.np/api/forex/v1/rates?from=${requestedFromStr}&to=${toStr}&page=1&per_page=100`;
+            const response = await fetch(url, { headers: { 'User-Agent': 'Excel-NRB-Addon' } });
+            if (!response.ok) return `#ERROR: HTTP ${response.status}`;
+            const json = await response.json();
+
+            const payload = json.data?.payload || json.data;
+            if (!Array.isArray(payload) || payload.length === 0)
+                return `#ERROR: No data from ${requestedFromStr} to ${toStr}`;
+
+            const results = extractRatesFromPayload(payload, targetCurrency, rateType, needsInversion);
+            if (results.length === 0) return `#ERROR: Currency ${currencyPair} not found`;
+
+            // Return as JSON string for parsing by NRB.PARSEJSON()
+            return JSON.stringify([["Date", "Rate"], ...results]);
+        }
+
+        // =================== SINGLE DATE HANDLING ===================
+        let currentDay = new Date(requestedFromStr);
+        for (let i = 0; i < 7; i++) {
+            const searchDateStr = formatDate(currentDay);
+            const url = `https://www.nrb.org.np/api/forex/v1/rates?from=${searchDateStr}&to=${searchDateStr}&page=1&per_page=1`;
+            const response = await fetch(url, { headers: { 'User-Agent': 'Excel-NRB-Addon' } });
+
+            if (response.ok) {
+                const json = await response.json();
+                const payload = json.data?.payload || json.data;
+                if (Array.isArray(payload) && payload.length > 0) {
+                    const results = extractRatesFromPayload(payload, targetCurrency, rateType, needsInversion);
+                    if (results.length > 0) {
+                        const finalRate = results[0][1];
+
+                        if (searchDateStr === requestedFromStr) {
+                            // Direct match: return positive number
+                            return finalRate;
+                        } else {
+                            // FALLBACK: return negative number
+                            return finalRate * -1; // <-- The requested change
+                        }
+                    }
+                }
+            }
+
+            currentDay.setDate(currentDay.getDate() - 1);
+        }
+
+        return `#ERROR: No rate found for ${currencyPair} on ${requestedFromStr} or 7 preceding days`;
+    } catch (err) {
+        return `#ERROR: ${err instanceof Error ? err.message : String(err)}`;
+    }
+}
+
+/**
+ * Helper function to parse JSON string back into table
+ * @customfunction
+ * @param {string} jsonString JSON string returned by NRB.FOREXRATE
+ * @returns {any[][]} Parsed table for Excel
+ * @alias NRB.PARSEJSON
+ */
+export function ParseJSON(jsonString: string): any[][] {
+    try {
+        const parsed = JSON.parse(jsonString);
+        if (Array.isArray(parsed) && Array.isArray(parsed[0])) return parsed;
+        return [["#ERROR: Invalid JSON structure"]];
+    } catch {
+        return [["#ERROR: Invalid JSON string"]];
+    }
+}
+
+
+// =====================================================================================
+// HELPER FUNCTIONS 
+// =====================================================================================
+
+/**
+ * Format date from various input types to 'yyyy-mm-dd' (UTC-safe)
+ */
+function formatDate(inputDate: any): string {
+    try {
+        if (inputDate === null || inputDate === undefined || inputDate === "") {
+            return "ERROR";
+        }
+
+        let dateObj: Date;
+
+        // 1️⃣ Already a Date object
+        if (inputDate instanceof Date) {
+            dateObj = inputDate;
+        }
+
+        // 2️⃣ Excel serial number (e.g., 45825)
+        else if (typeof inputDate === 'number') {
+            const MS_PER_DAY = 24 * 60 * 60 * 1000;
+            const excelEpoch = new Date(Date.UTC(1899, 11, 30));
+            dateObj = new Date(excelEpoch.getTime() + inputDate * MS_PER_DAY);
+        }
+
+        // 3️⃣ String input (handle many formats)
+        else if (typeof inputDate === 'string') {
+            const trimmed = inputDate.trim();
+            if (trimmed === "") return "ERROR";
+
+            // Normalize multiple spaces or mixed separators
+            let str = trimmed.replace(/[\s]+/g, ' ').replace(/[.]/g, '-').replace(/[\/]/g, '-');
+
+            // Handle DD-MMM-YYYY or YYYY-MMM-DD (e.g., 15-Oct-2025)
+            const monthNames = {
+                jan: "01", feb: "02", mar: "03", apr: "04", may: "05", jun: "06",
+                jul: "07", aug: "08", sep: "09", oct: "10", nov: "11", dec: "12"
+            };
+
+            const monthRegex = /(\b[a-zA-Z]{3,}\b)/;
+            if (monthRegex.test(str)) {
+                const parts = str.split(/[-\s]/);
+                const mapped = parts.map(p => {
+                    const lower = p.toLowerCase();
+                    if (monthNames[lower as keyof typeof monthNames]) {
+                        return monthNames[lower as keyof typeof monthNames];
+                    }
+                    return p;
+                });
+                str = mapped.join("-");
+            }
+
+            // Try YYYY-MM-DD
+            let isoMatch = str.match(/^(\d{4})-(\d{1,2})-(\d{1,2})$/);
+            if (isoMatch) {
+                const [_, y, m, d] = isoMatch;
+                dateObj = new Date(`${y}-${m.padStart(2, '0')}-${d.padStart(2, '0')}`);
+            }
+
+            // Try DD-MM-YYYY
+            else if (/^(\d{1,2})-(\d{1,2})-(\d{4})$/.test(str)) {
+                const [_, d, m, y] = str.match(/^(\d{1,2})-(\d{1,2})-(\d{4})$/)!;
+                dateObj = new Date(`${y}-${m.padStart(2, '0')}-${d.padStart(2, '0')}`);
+            }
+
+            // Fallback: Let JS try parsing (covers most locale formats)
+            else {
+                dateObj = new Date(str);
+            }
+        }
+
+        // 4️⃣ Unsupported type
+        else {
+            return "ERROR";
+        }
+
+        // 5️⃣ Validate
+        if (isNaN(dateObj.getTime())) {
+            return "ERROR";
+        }
+
+        // 6️⃣ Output normalized to YYYY-MM-DD
+        const year = dateObj.getUTCFullYear();
+        const month = String(dateObj.getUTCMonth() + 1).padStart(2, '0');
+        const day = String(dateObj.getUTCDate()).padStart(2, '0');
+
+        return `${year}-${month}-${day}`;
+    } catch {
+        return "ERROR";
+    }
+}
+
+
+/**
+ * Helper to extract rate from the payload array
+ */
+function extractRatesFromPayload(
+    payload: any[], 
+    targetCurrency: string, 
+    rateType: string, 
+    needsInversion: boolean
+): any[][] {
+    const results: any[][] = [];
+    const rateField = rateType === "B" ? "buy" : "sell";
+
+    for (const entry of payload) {
+        const dateStr = entry.date;
+        const rates = entry.rates;
+
+        if (!Array.isArray(rates)) continue;
+
+        for (const rateEntry of rates) {
+            const iso3 = rateEntry.currency?.iso3 || rateEntry.iso3;
+            
+            if (iso3 === targetCurrency) {
+                let rateValue = rateEntry[rateField];
+
+                if (rateValue === null || rateValue === undefined || rateValue === "") continue;
+
+                // Parse rate (remove commas if present)
+                const rateStr = String(rateValue).replace(/,/g, '');
+                const rateParsed = parseFloat(rateStr);
+
+                if (isNaN(rateParsed) || rateParsed <= 0) continue;
+
+                // Apply inversion if needed
+                const finalRate = needsInversion ? (1 / rateParsed) : rateParsed;
+                
+                results.push([dateStr, finalRate]);
+                break;
+            }
+        }
+    }
+    return results;
+}
+
+// =====================================================================================
+// Custom Function Association
+// =====================================================================================
+
+if (typeof CustomFunctions !== "undefined") {
+    CustomFunctions.associate("NRB.FOREXRATE", ForexRate); 
+}
